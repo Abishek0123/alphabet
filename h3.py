64610813ad9e6dc0f191947222b0ebff83463fc0
Abishek0123 <52314383+Abishek0123@users.noreply.github.com>
2019-08-07 15:18:37 +0530

    Create h3.py

---
+++ h3.py
@@ -0,0 +1,13 @@
+n=int(input())
+ls=input().split(" ")
+ls=[int(num) for num in ls]
+final=[]
+for x in range(0,num):
+  if(x==ls[x]):
+    final.append(ls[x])
+if not(len(final)==0):
+  final=sorted(final)
+  for i in range(0,len(final)):
+    print(final[i],end=' ')
+else:
+  print("-1")
