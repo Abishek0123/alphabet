ff0a37aad4c664a8b7a5cf14ad209bea4bc9ff05
Abishek0123 <52314383+Abishek0123@users.noreply.github.com>
2019-08-09 14:23:34 +0530

    Update 23h.py

--- 23h.py
+++ 23h.py
@@ -2,31 +2,31 @@ v,z=map(eval,input().split())
 aut=[]
 for k in range(v):
   aut.append(list(map(eval,input().split())))
-n=[]
-m=[]
-tmp_out=[]
-t=[]
-s=0
+p=[]
+o=[]
+temp_out=[]
+tt=[]
+k=0
 j=0
-m.append(aut[k][j])
-n.append([k,j])
-while [v-1,z-1] not in n:
+o.append(aut[k][j])
+p.append([k,j])
+while [v-1,z-1] not in p:
   k=0
-  for x in n:
+  for x in p:
     if x[0]+1<v and x[1]+1<z:
-      tmp_out.append(aut[x[0]+1][x[1]]+m[s])
-      tmp_out.append(aut[x[0]][x[1]+1]+m[s])
-      t.append([x[0]+1,x[1]])
-      t.append([x[0],x[1]+1])
+      temp_out.append(aut[x[0]+1][x[1]]+o[k])
+      temp_out.append(aut[x[0]][x[1]+1]+o[k])
+      tt.append([x[0]+1,x[1]])
+      tt.append([x[0],x[1]+1])
     elif x[0]+1<v:
-      tmp_out.append(aut[x[0]+1][x[1]]+m[s])
-      t.append([x[0]+1,x[1]])
+      temp_out.append(aut[x[0]+1][x[1]]+o[k])
+      tt.append([x[0]+1,x[1]])
     elif x[1]+1<z:
-      tmp_out.append(aut[x[0]][x[1]+1]+m[s])
-      t.append([x[0],x[1]+1])
-    s+=1
-  n=t
-  t=[]
-  out=tmp_out
-  tmp_out=[]
+      temp_out.append(aut[x[0]][x[1]+1]+o[k])
+      tt.append([x[0],x[1]+1])
+    k+=1
+  p=tt
+  tt=[]
+  out=temp_out
+  temp_out=[]
 print(max(out))
