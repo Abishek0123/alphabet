757f5e120c6a0ab2d5f4aa4c7a34e413ce89bb06
Abishek0123 <52314383+Abishek0123@users.noreply.github.com>
2019-08-09 14:00:27 +0530

    Create 21h.py

---
+++ 21h.py
@@ -0,0 +1,18 @@
+p,o=map(int,input().split())
+a=[]
+c1=[]
+for i in range(n):
+    li=[int(a) for ar in input().split()]
+    a.append(li)
+    if 0 in li:
+        m=li.index(0)
+        c1.append(m)
+for i in range(len(a)):
+    if 0 in a[i]:
+        for j in range(o):
+            a[i][j]=0
+for i in c1:
+    for j in range(p):
+        a[j][i]=0
+for i in a:
+    print(*i)
