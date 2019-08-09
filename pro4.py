8777e34c2d5164590863c7e43f0ce68fa5a93357
Abishek0123 <52314383+Abishek0123@users.noreply.github.com>
2019-08-09 14:51:47 +0530

    Update pro4.py

--- pro4.py
+++ pro4.py
@@ -1,11 +1,11 @@
-a,b=map(str,input().split())
-s=0
-if len(a)>len(b):
-  a,b=b,a
+v,z=map(str,input().split())
+y=0
+if len(v)>len(z):
+  v,z=z,v
 i=0
-while i<len(a):
-  s+=(ord(b[i])-ord(a[i]))
+while i<len(v):
+  y+=(ord(z[i])-ord(v[i]))
   i+=1
-for i in range(i,len(b)):
-  s+=ord(b[i])-ord('c')+1
-print(s)
+for i in range(i,len(z)):
+  y+=ord(z[i])-ord('a')+1
+print(y)
